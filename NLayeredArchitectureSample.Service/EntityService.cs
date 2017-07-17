0c66347ac999b4aa1649c292e9847ff259e6ef68
Ali <hasan.ali.karaca@hotmail.com>
2017-07-17 18:24:56 +0300

    update

--- NLayeredArchitectureSample.Service/EntityService.cs
+++ NLayeredArchitectureSample.Service/EntityService.cs
@@ -8,6 +8,14 @@ using NLayeredArchitectureSample.Repository;
 
 namespace NLayeredArchitectureSample.Service
 {
+    public interface IEntityService<T> : IService where T : BaseEntity
+    {
+        void Create(T entity);
+        void Delete(T entity);
+        IEnumerable<T> GetAll();
+        void Update(T entity);
+    }
+
     public abstract class EntityService<T> : IEntityService<T> where T : BaseEntity
     {
         IUnitOfWork _unitOfWork;
